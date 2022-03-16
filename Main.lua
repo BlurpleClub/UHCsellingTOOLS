1b18444c4db41cb67aea514063e9f3fd360127ae
BlurpleClub <96599798+BlurpleClub@users.noreply.github.com>
2022-03-16 11:59:35 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,13 @@
+-- Made by FinestAngel On https://forum.robloxscripts.com
+
+local args = {
+    [1] = "DropMoney",
+    [2] = "100" --Change the amount to the amount you wanna drop 
+}
+while true do
+game:GetService("ReplicatedStorage"):FindFirstChild(".gg/untitledhood"):FireServer(unpack(args))
+wait(0.1)
+end
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlurpleClub/UHCsellingTOOLS/main/Drop.lua"))()
